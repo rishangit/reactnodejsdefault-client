12de737e14687c7b0768b29205c80836bf466fd9
rishan <n.rishee@gmail.com>
2020-02-26 13:34:57 +0530

    add client

---
+++ src/components/Customers.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import{ map } from 'rxjs/operators'
+
+const Customers = props =>{
+
+let [customers, setCustomers] = useState([]);
+
+useEffect(()=>{
+    fetch('/api').then(data=>data.json())
+    .then(customers=>{
+        setCustomers(customers)
+        console.log(customers)
+      
+    })
+},[])
+
+    return (<div>
+        <div>This is customers</div>
+        <div>{customers.length}</div>
+            <div>{customers.map(({name, id})=>(<div key={id}>{name}</div>))}</div>
+        </div>)
+}
+
+export default Customers
